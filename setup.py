c6a40580f2190141019828011f93cbb74a453352
gyu-don <takumi.kt+git@gmail.com>
2019-11-30 21:45:36 +0900

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    install_requires = list(map(str.strip, f))
+
+setuptools.setup(
+    name = "blueqat_classicalbit_backend",
+    version="0.0.1",
+    author="gyu-don",
+    author_email="takumi.kt+blueqat@gmail.com",
+    description="Blueqat backend",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gyu-don/blueqat-classicalbit-backend",
+    license="Apache 2",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ]
+)
